15747756627312cc42b971ab535e43c8ae21e200
TristeroEmpire <diogene.le.cynique@gmail.com>
2015-10-12 14:05:06 +0200

    Some little enhancements
    - checking if the string "name" is empty or not
    - modifications about the documentation and some messages

--- Module2.fs
+++ Module2.fs
@@ -6,29 +6,30 @@ let checkAge name age =
     let field = name.ToString() + "," + age.ToString()
     //The loop return always a string type
     if age < 13 then  field + ", is a child"
-    elif  (age >= 13  && age < 20) then field + ", is a child"
+    elif  (age >= 13  && age < 20) then field + ", is a teenager."
     else field + ", is no longer a teenager."
 
-        
+
 [<EntryPoint>]
-let main argv = 
-    // loop : check if the user wants to continue or not 
+let main argv =
+    // loop : check if the user wants to continue or not
     let mutable loop = true
     while loop  do
         Console.Write("Give a name, please : ")
         let name = Console.ReadLine()
         Console.Write("How old is this person ? ")
-        //checking if the input is a number
+        //checking if the input is a number 
         let check, age  = System.Int32.TryParse(Console.ReadLine())
-        if check then 
-            //if the convertion happened, now the program use the function checkAge
+        // if check is the convertion happened and the string is not empty...
+        if (check && name.Length <> 0) then
+            // ... then the program use the function checkAge
             printfn "%s" (checkAge name age)
         else
             // Print a message if the input cannot be converted to an Int32
-            printfn "Error: you didn't give a number."
-        // continue or not 
-        Console.Write("Do you want to quit ? type 'q' to quit or any key to continue.")
+            printfn "Error: you didn't give a number or the name is empty."
+        // continue or not
+        Console.Write("Do you want to quit ? type 'q'+[Enter] to quit or any key+[Enter] to continue.")
         let again = Console.ReadLine()
         if again = "q" then loop <- false
-            
-    0 // retourne du code de sortie entier
+
+    0 // exit code
